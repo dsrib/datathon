0645173e8a8e1803a32334ac49c9010ae8de52cd
RaulCustodio <raulcustodiojunior@gmail.com>
2024-07-14 20:10:30 -0300

    chatgpt

--- datathon.py
+++ datathon.py
@@ -12,6 +12,8 @@ from sklearn.metrics import accuracy_score, mean_absolute_error, mean_squared_er
 from statsmodels.tsa.seasonal import seasonal_decompose
 #from prophet import Prophet  # Descomente se for usar o Prophet
 import openpyxl
+from openai import OpenAI
+OPENAI_API_KEY = "sk-proj-5n4QYv2WamHkLtefBuq3T3BlbkFJLcRVFLFC3ebEFXPIjgj4"
 
 st.set_page_config(layout='wide')
 
@@ -83,7 +85,41 @@ aba1, aba2 = st.tabs(['Visão Geral','Power BI'])
 
 with aba1:
     coluna1, coluna2, coluna3, coluna4, coluna5 = st.columns(5)
+        # Set OpenAI API key from Streamlit secrets
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+    # Set a default model
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+    # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
     # Ajuste as linhas abaixo de acordo com a disponibilidade dos dados
     #with coluna1:
     #     st.metric('Máximo', formata_numero(df0['Brent (F0B)'].max(), ''))
