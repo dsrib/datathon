8a4b385e20035ace88fa4611756e565ed5bd0c7d
RaulCustodio <raulcustodiojunior@gmail.com>
2024-07-28 13:50:26 -0300

    Update datathon.py

--- datathon.py
+++ datathon.py
@@ -14,7 +14,7 @@ import openpyxl
 from openai import OpenAI, error
 
 # Defina a chave da API do OpenAI diretamente ou use st.secrets
-OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", "sk-proj-YkvpYR2SHIGVs3meumFgT3BlbkFJlluu54tjfr6vAAOD3GdA")
+OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", "sk-proj-4HE1Em9uTJ7zYNgBkbmMT3BlbkFJtEos3KxsMmVdkedytjjh")
 
 st.set_page_config(layout='wide')
 
