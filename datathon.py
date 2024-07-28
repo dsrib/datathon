3b2b85b9c31092277b95a018b5ee6ea1d620b395
RaulCustodio <raulcustodiojunior@gmail.com>
2024-07-28 14:22:15 -0300

    Update datathon.py

--- datathon.py
+++ datathon.py
@@ -33,50 +33,48 @@ df_melted['indicador2'] = df_melted['indicador'].apply(lambda x: str(x[:-5]))
 
 # Visualização no Streamlit
 st.title('PASSOS MÁGICOS')
-selected_tab = st.tabs(['Visão Geral', 'Relatório Geral dos Alunos'])
+tabs = st.tabs(['Visão Geral', 'Relatório Geral dos Alunos'])
 
-if selected_tab == 'Visão Geral':
-    with selected_tab:
-        st.subheader("O que é a Passos Mágicos?")
-        st.markdown("A Passos Mágicos é uma instituição dedicada a promover o desenvolvimento acadêmico e pessoal dos alunos através de programas educativos e sociais.")
-        
-        # Sidebar com filtros (visível apenas na aba "Visão Geral")
-        with st.sidebar:
-            min_year, max_year = st.slider(
-                'Selecione o intervalo de anos',
-                min_value=int(df_melted['Ano'].min()),
-                max_value=int(df_melted['Ano'].max()),
-                value=(int(df_melted['Ano'].min()), int(df_melted['Ano'].max()))
-            )
-
-        filtered_df = df_melted[(df_melted['Ano'] >= min_year) & (df_melted['Ano'] <= max_year)]
-        df_g = filtered_df[filtered_df['indicador'].str.contains('INDE')]
-        df_g = df_g[df_g['value'].notna()]
-        df_g = df_g.drop_duplicates(subset=['Ano', 'NOME']).groupby(['Ano']).size().reset_index(name='Qtd Alunos')
-
-        # Gráficos
-        fig = go.Figure()
-        fig.update_layout(
-            width=800,  # Largura em pixels
-            height=500,  # Altura em pixels
+with tabs[0]:
+    st.subheader("O que é a Passos Mágicos?")
+    st.markdown("A Passos Mágicos é uma instituição dedicada a promover o desenvolvimento acadêmico e pessoal dos alunos através de programas educativos e sociais.")
+    
+    # Sidebar com filtros (visível apenas na aba "Visão Geral")
+    with st.sidebar:
+        min_year, max_year = st.slider(
+            'Selecione o intervalo de anos',
+            min_value=int(df_melted['Ano'].min()),
+            max_value=int(df_melted['Ano'].max()),
+            value=(int(df_melted['Ano'].min()), int(df_melted['Ano'].max()))
         )
 
-        # Adiciona os traços do gráfico
-        fig.add_trace(go.Bar(x=df_g['Ano'], y=df_g['Qtd Alunos'], name='Quantidade de Alunos', marker_color='midnightblue'))
-        fig.update_xaxes(type='category')  # Garantindo que o eixo x seja categórico
+    filtered_df = df_melted[(df_melted['Ano'] >= min_year) & (df_melted['Ano'] <= max_year)]
+    df_g = filtered_df[filtered_df['indicador'].str.contains('INDE')]
+    df_g = df_g[df_g['value'].notna()]
+    df_g = df_g.drop_duplicates(subset=['Ano', 'NOME']).groupby(['Ano']).size().reset_index(name='Qtd Alunos')
 
-        st.plotly_chart(fig)
-        st.table(df_g)
-        #chat_with_openai()
-elif selected_tab == 'Relatório Geral dos Alunos':
-    with selected_tab:
-        # Se aba2 está ativa, a sidebar é esvaziada
-        st.sidebar.empty()
-        st.title("Relatório de Desempenho dos Alunos da Passos Mágicos")
-        st.markdown("Este relatório tem por objetivo resumir os principais indicadores acadêmicos dos alunos da Passos Mágicos.")
+    # Gráficos
+    fig = go.Figure()
+    fig.update_layout(
+        width=800,  # Largura em pixels
+        height=500,  # Altura em pixels
+    )
 
-        # URL do relatório do Power BI
-        power_bi_report_url = "https://app.powerbi.com/view?r=eyJrIjoiM2Q1YWUzMjMtZjNmNC00ZGY4LWI3ZWUtYmY4N2FhNjc0M2Q3IiwidCI6ImNhZTdkMDYxLTA4ZjMtNDBkZC04MGMzLTNjMGI4ODg5MjI0YSIsImMiOjh9"
-        # Incorporando o relatório do Power BI usando um iframe
-        st.components.v1.iframe(power_bi_report_url, width=1000, height=600, scrolling=True)
+    # Adiciona os traços do gráfico
+    fig.add_trace(go.Bar(x=df_g['Ano'], y=df_g['Qtd Alunos'], name='Quantidade de Alunos', marker_color='midnightblue'))
+    fig.update_xaxes(type='category')  # Garantindo que o eixo x seja categórico
+
+    st.plotly_chart(fig)
+    st.table(df_g)
+
+with tabs[1]:
+    # Se aba "Relatório Geral dos Alunos" está ativa, a sidebar é esvaziada
+    st.sidebar.empty()
+    st.title("Relatório de Desempenho dos Alunos da Passos Mágicos")
+    st.markdown("Este relatório tem por objetivo resumir os principais indicadores acadêmicos dos alunos da Passos Mágicos.")
+
+    # URL do relatório do Power BI
+    power_bi_report_url = "https://app.powerbi.com/view?r=eyJrIjoiM2Q1YWUzMjMtZjNmNC00ZGY4LWI3ZWUtYmY4N2FhNjc0M2Q3IiwidCI6ImNhZTdkMDYxLTA4ZjMtNDBkZC04MGMzLTNjMGI4ODg5MjI0YSIsImMiOjh9"
+    # Incorporando o relatório do Power BI usando um iframe
+    st.components.v1.iframe(power_bi_report_url, width=1000, height=600, scrolling=True)
 
