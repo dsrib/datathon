9c1e921a740e95258d4365bdb57d9fe3979a3e7a
RaulCustodio <raulcustodiojunior@gmail.com>
2024-08-10 18:20:23 -0300

    Update datathon.py

--- datathon.py
+++ datathon.py
@@ -64,13 +64,13 @@ with tabs[0]:
         index=0  # Define o ano inicial selecionado
     )
     with coluna1:
-        st.metric(label="Alunos 2021", value=df_2020.value_counts(), delta=-0.5, delta_color="inverse")
+        st.metric(label="Alunos 2021", value=df_2020.shape[0], delta=0, delta_color="inverse")
     with coluna2:
-        st.metric(label="Alunos 2022", value=df_2021.value_counts(), delta=-0.5, delta_color="inverse")
+        st.metric(label="Alunos 2022", value=df_2021.shape[0], delta=df_2021.shape[0]/df_2020.shape[0], delta_color="inverse")
     with coluna3:
-        st.metric(label="Alunos 2023", value=df_2022.value_counts(), delta=-0.5, delta_color="inverse")
+        st.metric(label="Alunos 2023", value=df_2022.shape[0], delta=df_2022.shape[0]/df_2021.shape[0], delta_color="inverse")
     with coluna4:
-        st.metric(label="Gas price", value=4, delta=-0.5, delta_color="inverse")
+        st.metric(label="Indicador 4", value=4, delta=-0.5, delta_color="inverse")
     st.title("O que é a Passos Mágicos?")
     st.markdown(descricao)
     st.title("O que fazemos?") 
