44c4d9ade91cb15bb4064f6001bdbe807edd748f
RaulCustodio <raulcustodiojunior@gmail.com>
2024-08-10 17:42:50 -0300

    Update datathon.py

--- datathon.py
+++ datathon.py
@@ -64,19 +64,6 @@ with tabs[0]:
         options=indicadory,
         index=0  # Define o ano inicial selecionado
     )
-
-    with coluna1:
-        st.write("Histograma")
-        filtered_df = df_melted[(df_melted['Ano'] >= min_year) & (df_melted['Ano'] <= max_year)]
-        fig, df_g = visualizations.plot_students_per_year(filtered_df)
-        st.plotly_chart(fig, use_container_width=True)
-
-    with coluna2:
-        st.write("Indicadores")
-        filtered_df = df_melted[(df_melted['Ano'] >= min_year) & (df_melted['Ano'] <= max_year)]
-        fig2 = visualizations.scatter_plot(filtered_df, "INDE", "IAA", "INSTITUICAO", width=2000, height=400)
-         # Exibindo o gráfico com largura ajustável
-        st.plotly_chart(fig2, use_container_width=False, config={'responsive': True})
         
        
     st.title("O que é a Passos Mágicos?")
@@ -120,13 +107,27 @@ with tabs[0]:
     #st.table(df_g)
 
 with tabs[1]:
+
+    with coluna1:
+        st.write("Histograma")
+        filtered_df = df_melted[(df_melted['Ano'] >= min_year) & (df_melted['Ano'] <= max_year)]
+        fig, df_g = visualizations.plot_students_per_year(filtered_df)
+        st.plotly_chart(fig, use_container_width=True)
+
+    with coluna2:
+        st.write("Indicadores")
+        filtered_df = df_melted[(df_melted['Ano'] >= min_year) & (df_melted['Ano'] <= max_year)]
+        fig2 = visualizations.scatter_plot(filtered_df, "INDE", "IAA", "INSTITUICAO", width=2000, height=400)
+         # Exibindo o gráfico com largura ajustável
+        st.plotly_chart(fig2, use_container_width=False, config={'responsive': True})
+
     st.title("Relatório de Desempenho dos Alunos da Passos Mágicos")
     st.markdown("Este relatório tem por objetivo resumir os principais indicadores acadêmicos dos alunos da Passos Mágicos.")
 
     # URL do relatório do Power BI
     power_bi_report_url = "https://app.powerbi.com/view?r=eyJrIjoiM2Q1YWUzMjMtZjNmNC00ZGY4LWI3ZWUtYmY4N2FhNjc0M2Q3IiwidCI6ImNhZTdkMDYxLTA4ZjMtNDBkZC04MGMzLTNjMGI4ODg5MjI0YSIsImMiOjh9"
     # Incorporando o relatório do Power BI usando um iframe
-    st.components.v1.iframe(power_bi_report_url, width=600, height=600, scrolling=True)
+    st.components.v1.iframe(power_bi_report_url, width=1000, height=600, scrolling=True)
 
 
 with tabs[2]:
