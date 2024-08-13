aa3aa4f98038874b547efe4edbfdb15d8307b96a
RaulCustodio <raulcustodiojunior@gmail.com>
2024-08-13 19:50:15 -0300

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -118,8 +118,8 @@ def line_chart_column():
     fig2.update_layout(
         title='Quantidade Alunos ONG X População Embu-Guaçu',
         xaxis_title='Ano',
-        height=500,
-        width=500
+        height=100,
+        width=250
     )
     fig2.update_yaxes(title_text='%Populacao', secondary_y=False)
     fig2.update_yaxes(title_text='Populacao', secondary_y=True)
