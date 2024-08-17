06d9c7739433bad37190f23ab7ce6bf0b009700d
dsrib <dsribeiro58@gmail.com>
2024-08-17 01:57:15 -0300

    vai2

--- datathon.py
+++ datathon.py
@@ -265,4 +265,4 @@ with tabs[1]:
 
 with tabs[2]:
   piramide_etaria = pd.read_csv('Censo 2022 - Pirâmide etária - Embu-Guaçu (SP).csv', sep=';')
-  plot_piramide_etaria(piramide_etaria)
+  plot_piramide_etaria(piramide_etaria, pop_escolar)
