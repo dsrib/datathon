20b9fa9701038c6bd16e6ddea9a027f99c6b5bde
dsrib <dsribeiro58@gmail.com>
2024-08-17 02:54:35 -0300

    Update datathon.py

--- datathon.py
+++ datathon.py
@@ -312,6 +312,6 @@ with tabs[2]:
   qtde_alunos_idade = functions.ajuste(dados_alunos)
   st.header("Alunos da Passos Mágicos")
   try: plot_grafico_alunos(qtde_alunos_idade)
-    except Exception as e:
+  except Exception as e:
     st.exception(e)
 
